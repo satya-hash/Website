f8bb8f4a0d2e95955ffabcf17e88277daca559d9
satya <sathyagangadhara@gmail.com>
2022-09-09 09:36:03 +0530

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cardsContainer = document.querySelector('.cardsContainer')
 
 async function getProducts(){
-     let res = await fetch('https://fakestoreapi.com/products?limit=3')
+     let res = await fetch('https://dummyjson.com/products?limit=3')
      let data = await res.json();
      // console.log(data);
      return await data;
@@ -10,10 +10,10 @@ async function getProducts(){
 (async () => {
      let items;
      function showProduct(product) {
-          let { title, id, image , description} = product;
+          let { title, id, images , description} = product;
           return `<div class="product max-w-md  px-5 py-2 rounded overflow-hidden shadow-md" id=${id}>
                          <div class=' h-1/2 flex justify-center items-center'>
-                              <img class="w-1/2 h-full text-center" src=${image} alt=${title}>
+                              <img class="w-1/2 h-full text-center" src=${images[0]} alt=${title}>
                          </div>
                          <div class="px-6 py-4">
                               <div class="font-bold text-xl mb-2">${title}</div>
@@ -23,8 +23,11 @@ async function getProducts(){
                          </div>
                     </div>`;
      }
-     let products = await getProducts();
+     let { products } = await getProducts();
+     console.log(products);
      items = products.map(product => showProduct(product) )
      cardsContainer.innerHTML = items.join(' ');
 })()
+
+
 
